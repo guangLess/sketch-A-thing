5e79a20cfc3ffdb494723ac359778cfb96540324
guangLess <bha.guang@gmail.com>
2018-03-14 01:25:38 -0400

    drawing locally with color and size

--- sketch.js
+++ sketch.js
@@ -12,16 +12,14 @@ console.log("---->>", store)
 
 pTest = new Pixel(0,0,0)
 console.log("---->>", pTest)
-
+var colorV = 0
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.mousePressed(startPath)
   canvas.mouseReleased(endPath)
-
-  strokeWeight(10)
-  stroke(0)
-  noFill()
+  background(51)
+  smooth()
 }
 
 function startPath() {
@@ -51,24 +49,28 @@ function draw() {
     //if (currentPath.length === 5) currentPath = []
     currentPath.push(point)
 
-    console.log("---currentPath--->>", currentPath)
-
+    //console.log("---currentPath--->>", currentPath)
     if (currentPath.length > 2) {
       const pointx = currentPath.slice(-3)[0].x
       const pointy = currentPath.slice(-3)[0].y
 
       if (pointx !== point.x && pointy !== point.y) {
-        const eachPoint = new Pixel(point, [0, 0, 0], 3)
+        const currentColor = colorV
+        const eachColor = color((255- currentColor), (300 - currentColor), currentColor)
+        const strokeV = map(currentColor, 0, 255, 1, 30)
+        const eachPoint = new Pixel(point, eachColor, strokeV)
         store.drawPathWith(eachPoint)
+        colorV += 1
+
         //console.log(store.currentPath.getPath())
-        console.log("store ------>>",store.getState())
+        //console.log("store ------>>",store.getState())
 
       }
     }
+    if (colorV === 255) colorV = 0
   }
 
-  stroke(0);
-  strokeWeight(4);
+  //strokeWeight(2);
   noFill();
 /*  
   for (var i = 0; i < drawing.length; i++) {
@@ -80,17 +82,25 @@ function draw() {
     endShape();
   }
 */
-  
+//console.log("----😀-->>")
+
   for (var i = 0; i < store.getState().length; i++) {
     var path = store.getState()[i].getPath()
     beginShape()
     //console.log("------>>", path)
     for (var j = 0; j < path.length; j++) {
+
+      const colorData = path[j].color
+      const strokeData = path[j].stroke
+       stroke(colorData)
+       strokeWeight(strokeData)
+       //console.log("strokeV>>>>",strokeV)
+
        vertex(path[j].point.x, path[j].point.y)
+
      }
     endShape()
   }
 }
 
-//https://github.com/CodingTrain/website/blob/1c684717859e228684d7e2fed6f379e01d949328/CodingChallenges/CC_45_FirebaseSavingDrawing/sketch.js
 
