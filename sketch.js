e89867313061adc777534be1a356f7f7ea327664
guangLess <bha.guang@gmail.com>
2018-03-19 15:46:19 -0400

    retrive data

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+
+
 var drawing = []
 var currentPath = [];
 var isDrawing = false;
@@ -17,7 +20,17 @@ var drawRef = firebase.database().ref('drawing')
 //const testPix = new Pixel({x:10, y:10}, [0,0,0], 1)
 // drawRef.push(testPix)
 
-drawRef.on('value', snap => console.log("===> snap",snap.val()))
+const preDrawingData = []
+drawRef.once('value', snap => {
+    console.log("===> snap", snap.val())
+    snap.forEach( data => {
+      //console.log(data.key, data.val())
+      const fireValue = data.val()
+      const eachPix = new Pixel(fireValue.point, fireValue.color, fireValue.stroke)
+      preDrawingData.push(eachPix)
+    })
+  }
+)
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight)
@@ -26,6 +39,10 @@ function setup() {
   background(51)
   smooth()
   noFill()
+
+  strokeWeight(30)
+  stroke(255,255,255)
+  point(100,400)
 }
 
 function startPath() {
@@ -51,11 +68,11 @@ function draw() {
       x: mouseX,
       y: mouseY
     }
+
     //Fixme: currentPath should be called tracker? or move it somewhere else
     //if (currentPath.length === 5) currentPath = []
     currentPath.push(point)
 
-    //console.log("---currentPath--->>", currentPath)
     if (currentPath.length > 2) {
       const pointx = currentPath.slice(-3)[0].x
       const pointy = currentPath.slice(-3)[0].y
@@ -63,7 +80,7 @@ function draw() {
       if (pointx !== point.x && pointy !== point.y) {
         const currentColor = colorV
         //const eachColor = color((255- currentColor), (300 - currentColor), currentColor)
-        const eachColor = [(255- currentColor), (300 - currentColor), currentColor]
+        const eachColor = [(255 - currentColor), (300 - currentColor), currentColor]
         const strokeV = map(currentColor, 0, 255, 0.1, 25)
         const eachPoint = new Pixel(point, eachColor, strokeV)
         drawRef.push(eachPoint)
@@ -73,6 +90,9 @@ function draw() {
     }
     if (colorV === 250) colorV = 0
   }
+
+  // drawing with current status
+
   for (var i = 0; i < store.getState().length; i++) {
     var path = store.getState()[i].getPath()
     beginShape()
@@ -81,10 +101,29 @@ function draw() {
       const colorData = path[j].color
       const strokeData = path[j].stroke
       //const color = color(colorData[0], colorData[1], colorData[2])
-       stroke(color(colorData[0], colorData[1], colorData[2], 0.95))
-       strokeWeight(strokeData)
-       vertex(path[j].point.x, path[j].point.y)
-     }
+      stroke(color(colorData[0], colorData[1], colorData[2], 0.95))
+      strokeWeight(strokeData)
+      vertex(path[j].point.x, path[j].point.y)
+    }
+    endShape()
+  }
+  
+
+  // drawing from firebase 
+
+  if (preDrawingData.length > 1)
+  {
+      beginShape()
+    for (var j = 0; j < preDrawingData.length; j++) {
+      const colorData = preDrawingData[j].color
+      const strokeData = preDrawingData[j].stroke
+      //const color = color(colorData[0], colorData[1], colorData[2])
+      stroke(color(colorData[0], colorData[1], colorData[2], 0.95))
+      strokeWeight(strokeData)
+      vertex(preDrawingData[j].point.x, preDrawingData[j].point.y)
+
+      //point(preDrawingData[j].point.x, preDrawingData[j].point.y)
+    }
     endShape()
   }
 }
