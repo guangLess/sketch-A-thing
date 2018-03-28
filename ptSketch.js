1d4e7f6c2e05cad405b531af659a7aebbc58766a
guangLess <bha.guang@gmail.com>
2018-03-28 13:48:10 -0400

    added rect

--- ptSketch.js
+++ ptSketch.js
@@ -7,8 +7,18 @@ var space = new CanvasSpace("#hello");
 var form = space.getForm()
 
 space.add( () => {
-    form.point(space.pointer, 10)}
+    form.point(space.pointer, 10)
+}
 )
+
+space.add( (time, ftime) => {
+    var rect = Rectangle.fromCenter( space.center, space.size.$divide(2) )
+    var poly = Rectangle.corners( rect )
+    poly.shear2D( Num.cycle( time%5000/5000 ) - 0.5, space.center )
+
+    //form.fillOnly("#123").polygon( poly )
+    form.strokeOnly("#fff", 1).rect( rect )
+})
 
 space.play(200).bindMouse().bindTouch();
 
